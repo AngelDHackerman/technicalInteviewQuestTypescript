48245288bf66f13ee7a3b7295b860717b589ab28
angelDariaux <angeldariaux@gmail.com>
2023-08-30 22:37:52 -0600

    finished the stack challange

--- DataStructures/02_StackTyepscript.ts
+++ DataStructures/02_StackTyepscript.ts
@@ -31,5 +31,16 @@ class Stack<T> {
   size(): number {
     return this.items.length
   }
-
 }
+
+// Uso de la pila
+const stack = new Stack<number>();
+stack.push(1)
+stack.push(2)
+stack.push(3)
+console.log(stack)
+
+console.log(stack.pop()) // 3
+console.log(stack.peek())  // 2
+console.log(stack.isEmpty())  // false
+console.log(stack.size())  // 2
